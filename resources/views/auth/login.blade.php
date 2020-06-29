ad9b7acbf4805bda3e4a806bfeaa35b08174e708
CryDeTaan <CryDeTaan@users.noreply.github.com>
2020-06-29 08:32:01 +0200

    :lipstick: Made the Unauthenticated page a bit nicer.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,15 @@
 @extends('main')
 
 @section('body')
+    <div>
+        <div class="text-2xl mb-8 mt-4">C.R.U.D me like I am high</div>
 
-    {{-- Be carefull here, the x-crud.read componet is not escaping this message for he code class fo work. --}}
-    <p>
-        Please be sure to impersonate a user by selecting an 'Impersonations' option from the sidebar to the left.
-        This will simulate an authentication attempt and the authenticated user's information will display below.
-    </p>
+        <div class="text-l my-4">Unauthenticated</div>
+
+        <p>
+            Please be sure to impersonate a user by selecting an 'Impersonations' option from the sidebar to the left.
+            This will simulate an authentication attempt and the authenticated user's information will display below.
+        </p>
+    </div>
 
 @endsection
