39d722caf2cd08363a0479b298ab6d8874fba3ab
CryDeTaan <CryDeTaan@users.noreply.github.com>
2020-07-07 07:47:51 +0200

    :construction: Some more rewriting of the text on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,34 +71,85 @@
             create resource / article Owners and Contributors. So this is where an Entity Admin comes into play.
         </p>
         <div class="text-l my-4">Entity Admins</div>
+        <p>
+            The Entity Admins role will have ability to add Resource Owners and Resource Contributors for the Entity
+            they manage. Just a reminder, a resource in this example app is nothing more than an article or blog post.
+        </p>
+        <div class="text-l my-4">Resource Owners</div>
+        <p>
+            Resource Owner as the name suggests owns the resource, i.e. article or blog post. They have the ability to
+            Create, Read, Update, and Delete a resource. They also have the ability to assign Resource Contributors to
+            the Resources they own.
+        </p>
+        <div class="text-l my-4">Resource Contributors</div>
+        <p>
+            Resource Contributors can only contribute to resources they have been assigned to through the Read and
+            Update abilities.
+        </p>
+        <div class="text-l my-4">To sum up</div>
         <ol class="list-decimal mb-4 text-xs pl-10">
             <li>
-                Platform resources; admins, contributors, and then the entities. The entities are really what the
-                platform is about.
+                Platform: Platform Admins, Platform Contributors, and Entities.
             </li>
             <li>
-                Entity resources; entity admins, resources, resource owners, and resource contributors.
+                Entity: Entity Admins, Resources, Resource Owners, and Resource Contributors.
             </li>
         </ol>
         <p>
+
+            The roles are implemented in such a way that just because a user is a Platform Admin it doesn't allow them
+            to contribute to the Resources as well, like a Resource Contributor. No, a Platform Admin, administers the
+            platform. I hope that makes sense.
+        </p>
+        <div class="text-l my-4">Looking at it as a story</div>
+        <p>
             So, let me try create a scenario. I create a publication platform where companies can subscribe to.
-            The companies are considered the entities on this publications platform. Once a company subscribes,
-            a platform admin will create an entity resource representing the company and an entity admin, who will
-            managed the resources to this entity. And sure, it's not really necessary to have a platform admin add an
+            The companies are considered the Entities on this publications platform. Once a company subscribes,
+            a Platform Admin will create an Entity resource representing the company and an Entity Admin, who will
+            managed the resources to this Entity. And sure, it's not really necessary to have a Platform Admin add an
             entity, but I am trying to explain multi tenant permissions.
         </p>
         <p>
-            Once the entity and entity admin are created, the entity admin can go on and create resource owners.
-            Resources owners in this scenario could be considered a Managing Editor for the publication. The resource
-            owner can then create a resource, which essentially represents an article of sorts. The resource owner can
-            not only publish these mentioned articles, but also add resource contributors, for example an editor at the
-            company making use of the platform. The editor may only be allowed to make changes or additions to the
-            articles, but need to be reviewed and published by the resource owner.
+            Once the Entity and Entity Admin are created, the Entity Admin can go on and create Resource Owners and
+            Contributors. Resources Owners in this scenario could be considered a Managing Editor for the publication.
+            The Resource Owner can then create a Resource, which essentially represents an article of sorts. The
+            Resource Owner can not only publish these mentioned articles, but also assign Resource Contributors, for
+            example an editor at the company making use of the platform. The editor may only be allowed to make changes
+            or additions to the articles, but need to be reviewed and published by the Resource Owner.
         </p>
         <p>
-            The application must be built with the necessary permissions in mind. Meaning that entity resources should
-            not be accessible from other entity resources, i.e. entity admins, resource owners or contributors.
-            In addition, platform admins and contributors should not be able to actually access the entity resources.
+            The application must be built with the necessary permissions in mind. Meaning that Entity users should
+            not be accessible from other Entity Resources, i.e. Entity Admins, Resource Owners or Contributors.
+            In addition, Platform Admins and Contributors should not be able to actually access the Entity's Resources.
+        </p>
+        <div class="text-xl mb-4 mt-12"><span class="-ml-6 font-weight-light text-gray-700">#</span> Impersonating and
+            How to use
+        </div>
+        <p>
+            With the definition and scenario out of the way, let me quickly explain how to use this app. It is possible
+            to impersonate an user and then perform then actions on the different resources.
+        </p>
+        <p>
+            Impersonate an user by selecting an 'Impersonations' option from the sidebar to the left. This will simulate
+            an authentication attempt and the authenticated user's information will returned and displayed. Below is an
+            example of how an authenticated user's object is returned, which can be accessed in the view.
+        </p>
+        <p>
+            Once a User has been impersonated, it is possible to perform the action the selected user's role has
+            permission to perform. The action essentially dictates the which C.R.U.D. action to perform on a given
+            resource. For example, Impersonating the Platform Admin will allow the ability to add an Entity but will not
+            be allowed to add a Resource Contributor.
+        </p>
+        <div class="p-1 border rounded-md mb-2">
+            <pre><code class="text-xs bg-gray-200 php"><x-home.user/></code></pre>
+        </div>
+        <div class="text-xl mb-4 mt-12"><span class="-ml-6 font-weight-light text-gray-700">#</span> Model View
+            Control ++
+        </div>
+        <p>
+            In the following section I will try to explain the Model View Control Architecture and specifically how it
+            is translated into Laravel. I will also discuss some other areas needed to make this example application
+            function like Role-Based Access control, Policies and more.
         </p>
         <div class="text-l my-4">Note</div>
         <p>
@@ -107,100 +158,28 @@
             use of Route Groups. So please assume that everything I am showing isn't necessarily the best or
             only way of doing something :)
         </p>
-        <div class="text-xl mb-4 mt-12"><span class="-ml-6 font-weight-light text-gray-700">#</span> Impersonating</div>
-        <p>
-            Impersonate an user by selecting an 'Impersonations' option from the sidebar to the left. This will simulate
-            an authentication attempt and the authenticated user's information will display below. This is also how the
-            authenticated user's object is returned, and can be accessed:
-        </p>
-        <div class="p-1 border rounded-md">
-            <pre><code class="json text-xs">@include('data.user')</code></pre>
-        </div>
-
-        {{-- RBAC Description --}}
-        <div class="text-xl mb-4 mt-12"><span class="-ml-6 font-weight-light text-gray-700">#</span> Role Based Access
-            Control
-        </div>
-        <p>
-            Role based access control is achieved through Policies. Fortunately, Laravel has 'resourceful' policies for
-            actions on models when resourceful controllers are used.
-        </p>
-        <p>
-            To 'enable' this, the authorizeResource method in the should be added to the resourceful controller's
-            constructor; <code class="myCode">$this->authorizeResource(User::class, 'user');</code>. The
-            <code class="myCode">authorizeResource</code> method accepts the model's class name as its first argument,
-            and the name of the route / request parameter that will contain the model's ID as its second argument.
-        </p>
-        <p>
-            To have the required method signatures and type hints both the controller and the policy should be created
-            using the <code class="myCode">--model</code> flag. <br>For more information on Authorizing Resource
-            Controllers please see <a target="_blank" class="text-blue-500"
-                                      href="https://laravel.com/docs/7.x/authorization#via-controller-helpers">
-                Authorizing Resource Controllers</a> section from the Laravel Authorization Documentation.
-        </p>
-        {{-- Controller Constructor Code Block --}}
-        <div class="p-1 border rounded-md mb-2">
-            <pre><code class="text-xs bg-gray-200 php"><x-controllers.user.constructor/></code></pre>
-        </div>
-        <p>
-            Having the constructor defined will for example use the create method in the
-            <code class="myCode">App\Policies\UserPolicy</code> automatically when the create or store method in the
-            User Controller are called as they are both considered part of the Create.R.U.D action.
-        </p>
-        {{-- Policy Code Block --}}
-        <div class="p-1 border rounded-md mb-2">
-            <pre><code class="text-xs bg-gray-200 php"><x-policies.user.generic
-                        message="create models"
-                        method="create"
-                        action="create"
-                    /></code></pre>
-        </div>
-        <p>
-            At this point it should be noted that the User Model has a <code class="myCode">$user->abilities()</code>
-            property which is used to make sure the user attempting the action has the required ability.
-        </p>
-        <p>
-            There are two <code class="myCode">Many-to-Many</code> relationships at play here:
-        </p>
-        <ol class="list-decimal mb-4 text-xs pl-10">
-            <li>
-                User -> <a class="text-blue-500" href="/roles">Roles</a>: A User may be assigned many Roles
-            </li>
-            <li>
-                Role -> <a class="text-blue-500" href="/abilities">Abilities</a>: A Role may have many Abilities.
-            </li>
-        </ol>
-        <p>
-            To get to a user's abilities, I map over each role to get the abilities.
-        </p>
-        {{-- Policy Code Block --}}
-        <div class="p-1 border rounded-md mb-2">
-            <pre><code class="text-xs bg-gray-200 php"><x-models.user.roles/></code></pre>
-        </div>
-
 
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Route</div>
         <p>
-            Once a user is impersonated, its time to select an action. The action essentially dictates the which
-            C.R.U.D. action to perform on a given resource. And it all starts by accessing a route. This is the entry
-            point to the platform. The selected route will start executing the logic of the application.
+            As far a developer using the Laravel framework is concerned it all start here at the
+            <code class="myCode">routes/web.php</code> file. There is also <code class="myCode">routes/api.php</code>
+            and some other, but lets not worry about that for now. Obviously the Laravel frameworks takes care of a few
+            things for you when you visit a Laravel based Wep App but for now, just know that the route will start
+            executing the logic of the application and is defined in <code class="myCode">routes/web.php</code>. This is
+            the entry point to the application.
 
         </p>
         <p>
-            By visiting this URL, <code class="myCode">http://crud_2.0.test/entities/1</code>, the request will
-            initiated a
-            particular part of the application logic as the Route is registered in the
-            <code class="myCode">routes/web.php</code> file. This includes the HTTP method, which in this case is a
-            <code class="myCode">GET</code> Method, as well as the expected URI is specified. Finally, the
-            <code class="myCode">{entity}</code> parameter, in this case the ID of the requested Entity,
-            is include in the URI.
+            Lets use an example where by the Entity > Read action is selected on the Sidebar. This will instruct the
+            browser to navigate to <code class="myCode">http://crud_2.0.test/entities</code>, the request will
+            initiated a particular part of the application logic as the Route, <code class="myCode">entities</code>, is
+            requested. The route is registered in the <code class="myCode">routes/web.php</code> file. Further, as this
+            request is a standard <code class="myCode">GET</code> request, the Route is also defined with the expected
+            HTTP method. Finally, te second argument specifies the controller and the function to call. In this case the
+            <code class="myCode">index</code> method within the <code class="myCode">EntityController</code> controller.
         </p>
-        <p>
-            The second argument specifies the controller and the function to call. In this case the <code
-                class="myCode">show</code> method within the <code class="myCode">EntityController</code> controller.
-        </p>
-        <div class="p-1 border rounded-md">
-            <pre><code class="php text-xs">@include('data.route')</code></pre>
+        <div class="p-1 border rounded-md mb-2">
+            <pre><code class="text-xs bg-gray-200 php"><x-home.route/></code></pre>
         </div>
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Controller</div>
         <p>
@@ -300,5 +279,69 @@
             It it is important to note that you are not limited to using the Laravel Blade templating engine, the
             Laravel Application can act as a API 'service' which means data can be returned as a JSON payload as well.
         </p>
+        {{-- RBAC Description --}}
+        <div class="text-xl mb-4 mt-12"><span class="-ml-6 font-weight-light text-gray-700">#</span> Role Based Access
+            Control
+            <p>
+                Role based access control is achieved through Policies. Fortunately, Laravel has 'resourceful' policies
+                for
+                actions on models when resourceful controllers are used.
+            </p>
+            <p>
+                To 'enable' this, the authorizeResource method in the should be added to the resourceful controller's
+                constructor; <code class="myCode">$this->authorizeResource(User::class, 'user');</code>. The
+                <code class="myCode">authorizeResource</code> method accepts the model's class name as its first
+                argument,
+                and the name of the route / request parameter that will contain the model's ID as its second argument.
+            </p>
+            <p>
+                To have the required method signatures and type hints both the controller and the policy should be
+                created
+                using the <code class="myCode">--model</code> flag. <br>For more information on Authorizing Resource
+                Controllers please see <a target="_blank" class="text-blue-500"
+                                          href="https://laravel.com/docs/7.x/authorization#via-controller-helpers">
+                    Authorizing Resource Controllers</a> section from the Laravel Authorization Documentation.
+            </p>
+            {{-- Controller Constructor Code Block --}}
+            <div class="p-1 border rounded-md mb-2">
+                <pre><code class="text-xs bg-gray-200 php"><x-controllers.user.constructor/></code></pre>
+            </div>
+            <p>
+                Having the constructor defined will for example use the create method in the
+                <code class="myCode">App\Policies\UserPolicy</code> automatically when the create or store method in the
+                User Controller are called as they are both considered part of the Create.R.U.D action.
+            </p>
+            {{-- Policy Code Block --}}
+            <div class="p-1 border rounded-md mb-2">
+            <pre><code class="text-xs bg-gray-200 php"><x-policies.user.generic
+                        message="create models"
+                        method="create"
+                        action="create"
+                    /></code></pre>
+            </div>
+            <p>
+                At this point it should be noted that the User Model has a <code
+                    class="myCode">$user->abilities()</code>
+                property which is used to make sure the user attempting the action has the required ability.
+            </p>
+            <p>
+                There are two <code class="myCode">Many-to-Many</code> relationships at play here:
+            </p>
+            <ol class="list-decimal mb-4 text-xs pl-10">
+                <li>
+                    User -> <a class="text-blue-500" href="/roles">Roles</a>: A User may be assigned many Roles
+                </li>
+                <li>
+                    Role -> <a class="text-blue-500" href="/abilities">Abilities</a>: A Role may have many Abilities.
+                </li>
+            </ol>
+            <p>
+                To get to a user's abilities, I map over each role to get the abilities.
+            </p>
+            {{-- Policy Code Block --}}
+            <div class="p-1 border rounded-md mb-2">
+                <pre><code class="text-xs bg-gray-200 php"><x-models.user.roles/></code></pre>
+            </div>
+        </div>
     </div>
 @endsection
