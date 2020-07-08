34aba44c0640ffd3e7c6838b79dcde7c931d01c3
CryDeTaan <CryDeTaan@users.noreply.github.com>
2020-07-08 18:05:26 +0200

    :hammer: Moving the roles section to a component

--- resources/views/actions/user/create.blade.php
+++ resources/views/actions/user/create.blade.php
@@ -77,21 +77,7 @@
 
         {{-- Role Description --}}
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Role</div>
-        <p>
-            Because all the different user type actions are performed using the same User Controller and Model, I had
-            to make sure that the action performed on the user type is allowed based on the authenticated user's role.
-        </p>
-        <p>
-            To achieve this, a Role Policy was created and defined as below. Take note of the requested role,
-            <code class="myCode">{{ request()->actionOn }}</code>, and which
-            <code class="myCode">$user->roles->contains('name','{role}')</code> rule will return true. A user's role
-            property(<code class="myCode">$user->roles</code>) should contain the role required to perform the action.
-        </p>
-
-        {{-- Role Code Block --}}
-        <div class="p-1 border rounded-md mb-2">
-            <pre><code class="text-xs bg-gray-200 php"><x-policies.user.role/></code></pre>
-        </div>
+        <x-policies.user.role-section/>
 
         {{-- Controller Description --}}
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Controller</div>
