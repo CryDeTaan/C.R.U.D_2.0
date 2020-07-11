79b4e7c7a21a28d17f3b0a3fb236d506c064b8ce
CryDeTaan <CryDeTaan@users.noreply.github.com>
2020-07-11 17:50:44 +0200

    :pencil: Some work done on Resource read view

--- resources/views/actions/resource/read.blade.php
+++ resources/views/actions/resource/read.blade.php
@@ -12,16 +12,20 @@
             In this page I am listing all the resource, and selecting a resource from below will take us to a specific
             resource.
         </p>
+        <p>
+            I am covering both the options mentioned above so it should also then clear up that although both are
+            essentially the Read part of C.R.U.D, they logic is slightly different and will be seen in the Route,
+            Controller, Model, and View descriptions which follows.
+        </p>
 
         {{-- Resource Table Description --}}
         <div class="text-xl mb-4 mt-12">
-            <span class="-ml-6 text-gray-700">#</span> {{ slug_to_title(request()->actionOn) }}s
+            <span class="-ml-6 text-gray-700">#</span> {{ slug_to_titles(request()->actionOn) }}
         </div>
         <p>
-            Below is a list of the {{ slug_to_title(request()->actionOn) }}s. As with the
-            other action pages, I'll explain how the list
-            of {{ slug_to_title(request()->actionOn) }}s
-            were returned by explaining each area of importance in a bit more detail.
+            Below is a list of the {{ slug_to_titles(request()->actionOn) }}. As with the other action pages, I'll
+            explain how the list of {{ slug_to_titles(request()->actionOn) }} were returned by explaining each area of
+            importance in a bit more detail.
         </p>
 
         {{-- Resource Table --}}
@@ -30,9 +34,9 @@
         {{-- Route Description --}}
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Route</div>
         <p>
-            To receive a view containing a list of {{ slug_to_title(request()->actionOn) }}s the
-            <code class="myCode">{{ request()->url() }}</code> URL was requested using the
-            <code class="myCode">{{ request()->method() }}</code> method, therefor the defined route for this request in
+            To receive a view containing a list of {{ slug_to_titles(request()->actionOn) }} a
+            <code class="myCode">{{ request()->method() }}</code> request was made to
+            <code class="myCode">{{ request()->url() }}</code>, therefor the defined route for this request in
             <code class="myCode">routes/web.php</code> is as follow:
         </p>
 
@@ -46,30 +50,14 @@
                     /></code></pre>
         </div>
 
-        {{-- Policy Description --}}
-        <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Policy</div>
-        <p>
-            To perform this action the authenticated user should have the <code class="myCode">read_resource</code>
-            Ability and is authorised by the <code class="myCode">viewAny</code> Policy method as follow:
-        </p>
-
-        {{-- Policy Code Block --}}
-        <div class="p-1 border rounded-md mb-2">
-            <pre><code class="text-xs bg-gray-200 php"><x-policies.generic
-                        className="Resource"
-                        message="view any model"
-                        method="viewAny"
-                        ability="read_resource"
-                    /></code></pre>
-        </div>
-
         {{-- Controller Description --}}
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Controller</div>
         <p>
             The <code class="myCode">index</code> method in the <code class="myCode">ResourceController</code> will
             handel this request. And although the controller is for all resources, I want to control which resources are
             returned based on the entity, and in this case I only want to return the resources for the
-            {{ auth()->user()->entity->name }} entity, as the currently signed in user belongs to this entity.
+            {{ auth()->user()->entity->name }} entity, as the currently signed in user,{{ auth()->user()->name }},
+            belongs to this entity.
         </p>
         <p>
             Because of the <code class="myCode">Many-to-Many</code> relationship between Resources and a User (as can be
@@ -94,12 +82,29 @@
         </p>
         <p>
             Resources can have many Users and are Authorised certain actions on the resource based on the User's role.
-            Then a Resource belongs to a User which indicates the Resource Owner.
+            However a Resource belonging to a User which indicates the Resource Owner.
         </p>
 
         {{-- Model Code Block --}}
         <div class="p-1 border rounded-md">
             <pre><code class="text-xs bg-gray-200 php"><x-models.resource.index/></code></pre>
+        </div>
+
+        {{-- Policy Description --}}
+        <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Policy</div>
+        <p>
+            To perform this action the authenticated user should have the <code class="myCode">read_resource</code>
+            Ability and is authorised by the <code class="myCode">viewAny</code> Policy method as follow:
+        </p>
+
+        {{-- Policy Code Block --}}
+        <div class="p-1 border rounded-md mb-2">
+            <pre><code class="text-xs bg-gray-200 php"><x-policies.generic
+                        className="Resource"
+                        message="view any model"
+                        method="viewAny"
+                        ability="read_resource"
+                    /></code></pre>
         </div>
 
         {{-- View Description --}}
@@ -111,16 +116,16 @@
             this page.
         </p>
         <p>
-            The <code class="myCode">$resource</code> object will only contains the resources for the entity of the user
-            making the request. This object is also used to "build" the table at the start of the page using the Laravel
-            Blade Control Structure; <code class="myCode">&#64;foreach ($resources as $resource)</code>.
+            The <code class="myCode">$resource</code> object will only contain the resources of the user making the
+            request. This object is also used to "build" the table at the start of the page using the Laravel Blade
+            Control Structure; <code class="myCode">&#64;foreach ($resources as $resource)</code>.
         </p>
         <p>
-            As mentioned in the Model section, the belongsTo relationship allows me to retrieve the user for a give
-            resource, i.e. <code class="myCode">$resource->user</code>. The $resource object includes this because when
-            I build the table using the <code class="myCode">&#64;foreach</code> loop, one of the columns are defined by
-            <code class="myCode">$resource->user->name</code> and this will then automatically include the related
-            dynamic property as part of the object.
+            As mentioned in the Model section, the <code class="myCode">belongsTo</code> relationship allows me to
+            retrieve the user for a give resource, i.e. <code class="myCode">$resource->user</code>. The $resource
+            object includes this because when I build the table using the <code class="myCode">&#64;foreach</code> loop,
+            one of the columns are defined by <code class="myCode">$resource->user->name</code> and this will then
+            automatically include the related dynamic property as part of the object.
         </p>
         <p>
             Something else to notice is that the user object also includes its relationship to roles, but I never
