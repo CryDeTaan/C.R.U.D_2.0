a88fd95290ce153e2b9ae483ea8a47728198a4bd
CryDeTaan <CryDeTaan@users.noreply.github.com>
2020-07-11 18:10:26 +0200

    :pencil: Some work done on Resource show components

--- resources/views/actions/resource/show.blade.php
+++ resources/views/actions/resource/show.blade.php
@@ -6,21 +6,22 @@
         <div class="text-2xl mb-6 mt-4">C.Read.U.D {{ slug_to_title(request()->actionOn) }}</div>
 
         <p>
-            As mentioned in the previous page the <code class="myCode">R</code> in C.R.U.D. is for Reading resources.
-            In this case though, I am only reading a single Resource. Meaning, Reading a resource is either reading all
-            resources or a single resource, like in this case i am reading a single resource. Albeit the same, the
-            process/logic to get a single resource is slightly different as outlined below.
+            As mentioned in the <a class="text-blue-500" href="{{ url()->previous() }}">previous</a> page the
+            <code class="myCode">R</code> in C.R.U.D. is for Reading resources. In this case though, I am only reading a
+            single Resource. Meaning, Reading a resource is either reading all resources or a single resource, like in
+            this case i am reading a single resource. Albeit the same, the process/logic to get a single resource is
+            slightly different as outlined below.
         </p>
 
         {{-- Route Description --}}
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Route</div>
         <p>
-            To receive a view containing only the selected resource, the
-            <code class="myCode">{{ request()->url() }}</code> URL was requested using the
-            <code class="myCode">{{ request()->method() }}</code> method. What is important to note here is that the
+            To receive a view containing only the selected resource, a
+            <code class="myCode">{{ request()->method() }}</code> request was made to
+            <code class="myCode">{{ request()->url() }}</code>. What is important to note here is that the
             route in the <code class="myCode">routes/web.php</code> is defined to include a
             <code class="myCode">{parameter}</code>. This will pass the parameter to the controller, in this case the ID
-            of the Resource. With this the controller can easily obtain an instance of the resource through something
+            of the Entity. With this the controller can easily obtain an instance of the resource through something
             Laravel calls Route Model Binding.
         </p>
 
@@ -34,11 +35,53 @@
                     /></code></pre>
         </div>
 
+        {{-- Controller Description --}}
+        <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Controller</div>
+        <p>
+            As mentioned above, the <code class="myCode">show</code> method in the
+            <code class="myCode">{{ slug_to_controller(request()->actionOn) }}Controller</code>
+            will receive the ID of the requested resource and obtain an instance of it using Route Model Binding which
+            provides a convenient way to automatically inject a model instances directly into the controller. This can
+            be seen in the way the <code class="myCode">public function show(Resource $resource)</code>.
+        </p>
+
+        {{-- Controller Code Block --}}
+        <div class="p-1 border rounded-md">
+            <pre><code class="text-xs bg-gray-200 php"><x-controllers.resource.show/></code></pre>
+        </div>
+
+        {{-- Model Description --}}
+        <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Model</div>
+        <p>
+            There is really nothing special required in the Model to return a requested resource using Route Model
+            Binding. But remember the model has dynamic Eloquent properties and can be accessed as displayed below:
+        </p>
+        <ol class="list-decimal mb-4 text-xs pl-10">
+            <li>
+                <code class="myCode">return $this->belongsTo(entity::class);</code> -
+                <code class="myCode">$resource->entity;</code>
+            </li>
+            <li>
+                <code class="myCode">return $this->belongsTo(user::class);</code> -
+                <code class="myCode">$resource->user;</code>
+            </li>
+            <li>
+                <code class="myCode">return $this->belongsToMany(user::class);</code> -
+                <code class="myCode">$resource->users;</code>
+            </li>
+        </ol>
+
+        {{-- Model Code Block --}}
+        <div class="p-1 border rounded-md">
+            <pre><code class="text-xs bg-gray-200 php"><x-models.resource.show/></code></pre>
+        </div>
+
         {{-- Policy Description --}}
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Policy</div>
         <p>
             To perform this action the authenticated user should have the <code class="myCode">read_resource</code>
-            Ability and is authorised by the <code class="myCode">view</code> Policy method as follow:
+            Ability and should be one of the resource's contributors which is authorised by the <code class="myCode">view</code>
+            Policy method as follow:
         </p>
 
         {{-- Policy Code Block --}}
@@ -51,35 +94,6 @@
                         secondCheck="true"
                         ability="read_resource"
                     /></code></pre>
-        </div>
-
-        {{-- Controller Description --}}
-        <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Controller</div>
-        <p>
-            As mentioned above, the <code class="myCode">show</code> method in the
-            <code class="myCode">{{ slug_to_controller(request()->actionOn) }}Controller</code>
-            will receive the ID of the requested resource and obtain an instance of it using Route Model Binding as can
-            be seen by the way the function is declared,
-            <code class="myCode">public function show(Resource $resource)</code>.
-        </p>
-
-        {{-- Controller Code Block --}}
-        <div class="p-1 border rounded-md">
-            <pre><code class="text-xs bg-gray-200 php"><x-controllers.resource.show/></code></pre>
-        </div>
-
-        {{-- Model Description --}}
-        <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Model</div>
-        <p>
-            There is really nothing special required in the Model to return a requested resource using Route Model
-            Binding. But remember the model has a dynamic Eloquent properties,
-            <code class="myCode">return $this->belongsTo(user::class);</code> and although I am not using it in this
-            view, should I change my mind it is as easy as <code class="myCode">$resource->user;</code>.
-        </p>
-
-        {{-- Model Code Block --}}
-        <div class="p-1 border rounded-md">
-            <pre><code class="text-xs bg-gray-200 php"><x-models.resource.show/></code></pre>
         </div>
 
         {{-- View Description --}}
--- resources/views/components/models/resource/show.blade.php
+++ resources/views/components/models/resource/show.blade.php
@@ -6,13 +6,33 @@ class Resource extends Model
 {
 
     /**
-    * A Resource will belong to a Resource Owner(User).
-    *
-    * @return  \Illuminate\Database\Eloquent\Relations\BelongsTo
-    */
+     * A Resource will belong to a Resource Owner(User).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(user::class);
+    }
+
+    /**
+     * A Resource will belong to a Resource Owner(User).
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function entity()
+    {
+        return $this->belongsTo(entity::class);
+    }
+
+    /**
+     * A resource may be assigned to many users.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
     }
 
 }
