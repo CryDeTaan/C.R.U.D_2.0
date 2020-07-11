afc0043bfb59ca78af5df8091f52388205cb1e63
CryDeTaan <CryDeTaan@users.noreply.github.com>
2020-07-11 15:56:54 +0200

    :pencil: Some work done on Entity edit view

--- resources/views/actions/entity/edit.blade.php
+++ resources/views/actions/entity/edit.blade.php
@@ -15,15 +15,15 @@
 
         {{-- Resource Table Description --}}
         <div class="text-xl mb-4 mt-12">
-            <span class="-ml-6 text-gray-700">#</span>
-            {{ Str::of(slug_to_title(request()->actionOn))->plural() }}
+            <span class="-ml-6 text-gray-700">#</span> {{ slug_to_titles(request()->actionOn) }}
         </div>
         <p>
-            Below is a list of {{ slug_to_title(request()->actionOn) }}s and selecting one will update the form below
-            which will be used to update the Entity. This is not generally included in the Edit view, but I did it to
-            make it a bit easier to select a resource to update. Where as the <a href="#">Read</a> action will only show
-            a resource. However, this is sometimes combined so that when viewing a resource, there would be a edit, and
-            that edit button is what will return this view, the view to Edit, which contains a form, like I have below.
+            Below is a list of {{ slug_to_titles(request()->actionOn) }} and selecting one will update the form below
+            which will be used to update the Entity. A Table is not generally included in the Edit view, but I did it to
+            make it a bit easier to select a resource to update. Where as the
+            <a class="text-blue-500" href="/entities/1">Read</a> action will only show a resource. However, this is
+            sometimes combined so that when viewing a resource, there may be a edit button, and that edit button is what
+            will return this view, the view to Edit, which contains a form, like I have below.
         </p>
 
         {{-- Resource Table --}}
@@ -47,17 +47,33 @@
             button="Update"
         ></x-entity-form>
 
+        <p class="mt-4">
+            Because HTML forms can't make PUT requests, a hidden _method field(<code
+                class="myCode">&#64;method('PUT')</code>) Blade directive to spoof the required HTTP verbs is included
+            in the form. In addition, similar to when creating/storing a resource, an anti cross-site request forgery
+            _token(<code class="myCode">&#64;csrf</code>) Blade directive is also required to be sent as part of the
+            form. This creates two hidden input field for the form:<br>
+            1. <code class="myCode">&lt;input type="hidden" name="_method" value="PUT"&gt;</code>, and <br>
+            2. <code class="myCode">&lt;input type="hidden" name="_token" value="{{ csrf_token() }}"&gt;</code>
+        </p>
+        <p>
+            <br> More information available from the Laravel Documentation for
+            <a target="_blank" class="text-blue-500" href="https://laravel.com/docs/7.x/csrf">CSRF Protection</a> and
+            <a target="_blank" class="text-blue-500" href="https://laravel.com/docs/7.x/blade#method-field">Method
+                Fields</a>.
+        </p>
+
         {{-- Route Description --}}
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Route</div>
         <p>
-            This page was reached by visiting the following URL using the
-            <code class="myCode">{{ request()->method() }}</code> method:
+            This page was reached with a <code class="myCode">{{ request()->method() }}</code> request to
             <code class="myCode">{{ request()->url() }}</code>.
         </p>
         <p>
-            Take note of the parameter sent through, similar to showing an Entity resource, the parameter in the URI
-            will be used in the controller to obtain the requested resource. The process of returning this view start by
-            matching the request to a definition in the <code class="myCode">routes/web.php</code> as follow:
+            Take note of the <code class="myCode">{parameter}</code> sent through, similar to showing an Entity
+            resource, the parameter in the URI will be used by the controller to obtain the requested resource using
+            Laravel's Route Model Binding. The process of returning this view start by matching the request to a
+            definition in the <code class="myCode">routes/web.php</code> as outlined below:
         </p>
 
         {{-- Route Code Block --}}
@@ -70,10 +86,29 @@
                     /></code></pre>
         </div>
 
+        {{-- Controller Description --}}
+        <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Controller</div>
+        <p>
+            The <code class="myCode">edit</code> method in the <code class="myCode">EntityController</code> will
+            handel this request and, again, we will need a form with the requested user object to be updated. Keep in
+            mind that it is not really necessary to include the object, but it does improve the user experience.
+        </p>
+        <p>
+            Similarly to the <a class="text-blue-500"
+                                href="{{ url('entities/' . $entity->id . '?actionOn=' . request()->actionOn) }}">show</a>
+            method, the edit method also makes use of Route Model Binding to obtain the resource object which will be
+            included when returning the view.
+        </p>
+
+        {{-- Controller Code Block --}}
+        <div class="p-1 border rounded-md">
+            <pre><code class="text-xs bg-gray-200 php"><x-controllers.entity.edit/></code></pre>
+        </div>
+
         {{-- Policy Description --}}
         <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Policy</div>
         <p>
-            To perform this action the authenticated user should have the <code class="myCode">update_resource</code>
+            To perform this action the authenticated user should have the <code class="myCode">update_entity</code>
             Ability and is authorised by the <code class="myCode">update</code> Policy method as follow:
         </p>
 
@@ -85,23 +120,6 @@
                         method="update"
                         ability="update_entity"
                     /></code></pre>
-        </div>
-
-        {{-- Controller Description --}}
-        <div class="text-xl mb-4 mt-12"><span class="-ml-6 text-gray-700">#</span> Controller</div>
-        <p>
-            The <code class="myCode">edit</code> method in the <code class="myCode">EntityController</code> will
-            handel this request and, again, we will need a form with the requested user object to be updated. Keep in
-            mind that it is not really necessary to include the object, but it does improve the user experience.
-        </p>
-        <p>
-            Similarly to the show method, the edit method also makes use of Route Model Binding to obtain the resource
-            object which will be included when returning the view.
-        </p>
-
-        {{-- Controller Code Block --}}
-        <div class="p-1 border rounded-md">
-            <pre><code class="text-xs bg-gray-200 php"><x-controllers.entity.edit/></code></pre>
         </div>
 
         {{-- View Description --}}
