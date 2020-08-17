7c8d166279cbeed089f758c91e2267a5bab0930d
CryDeTaan <CryDeTaan@users.noreply.github.com>
2020-08-17 05:37:40 +0200

    addedd throttling on web routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            'throttle:20,3',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
