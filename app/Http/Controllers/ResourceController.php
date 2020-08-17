c1a9ebc23b8ed1022985a33ea6a98512ffb29882
CryDeTaan <CryDeTaan@users.noreply.github.com>
2020-08-17 21:05:16 +0200

    Update resource contributor as well

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -108,9 +108,14 @@ class ResourceController extends Controller
         $validatedAttributes = request()->validate([
             'name'      => ['required', 'string', 'max:255'],
             'field'     => ['required', 'string', 'max:255'],
+            'resource_contributor'  => ['exists:users,id']
         ]);
 
+        $resource_contributor = User::find($validatedAttributes['resource_contributor']);
+        $this->authorize('assign', $resource_contributor);
+
         $resource->update($validatedAttributes);
+        $resource->assignUser($resource_contributor);
 
         return view('actions.resource.update', compact('resource'));
     }
